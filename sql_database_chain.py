20faea01af510760af7b2ffec3188f56e9c155ec
Eduardo Hilário <eduhilario@proton.me>
2023-10-30 14:12:28 -0300

    refactor: Remove unnecessary code and comments, simplify query

--- sql_database_chain.py
+++ sql_database_chain.py
@@ -18,26 +18,26 @@ API_KEY = env("OPENAI_API_KEY")
 llm = ChatOpenAI(temperature=0, openai_api_key=API_KEY)
 db = setup_database()
 
-connection = connect_to_database()
-cursor = connection.cursor()
-if connection:
-    intended_tables = [
-        "bi_dimensao_tempo",
-        "bi_dimensao_tempo_diario",
-        "bi_fato_previsao",
-        "bi_fato_ressuprimento",
-        "bi_fato_venda",
-        "bi_fato_venda_cliente",
-        "bi_fato_venda_diario",
-    ]
+# connection = connect_to_database()
+# cursor = connection.cursor()
+# if connection:
+#     intended_tables = [
+#         "bi_dimensao_tempo",
+#         "bi_dimensao_tempo_diario",
+#         "bi_fato_previsao",
+#         "bi_fato_ressuprimento",
+#         "bi_fato_venda",
+#         "bi_fato_venda_cliente",
+#         "bi_fato_venda_diario",
+#     ]
 
-    matching_tables = get_matching_tables(intended_tables)
-    matching_tables_str = ", ".join(matching_tables)
-    if matching_tables:
-        print(f"Using tables: {', '.join(matching_tables)}")
-    error_tables = get_error_tables(intended_tables)
-    if error_tables:
-        print(f"Warning! These tables were not found: {', '.join(error_tables)}")
+#     matching_tables = get_matching_tables(intended_tables)
+#     matching_tables_str = ", ".join(matching_tables)
+#     if matching_tables:
+#         print(f"Using tables: {', '.join(matching_tables)}")
+#     error_tables = get_error_tables(intended_tables)
+#     if error_tables:
+#         print(f"Warning! These tables were not found: {', '.join(error_tables)}")
 
 QUERY = """
 Given an input question, first create a syntactically correct MYSQL query to run, then look at the results of the query and return the answer. Use the following format:
@@ -46,8 +46,8 @@ SQLQuery: [SQL query to run]
 SQLResult: [result of the SQLQuery]
 Answer: [final answer here]
 {question}
-You'll only use the following tables on your reasoning: {matching_tables_str}
-The column `tempo_id` is the foreign key to the table `bi_dimensao_tempo_diario`.
+The invoices and values for sales are on the table `faturamento_nfe`.
+When asked for invoices use faturamento_nfe_situacao_id <> 1.
 Return JSON in a single-line without whitespaces
 The final answer MUST be in Brazilian Portuguese
 Infer the suffix from the context if there's a final value as an answer
@@ -61,20 +61,15 @@ BEGIN!
 
 db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm, db=db, verbose=True)
 output_parser = StrOutputParser()
-question = "Qual a quantidade total de ressuprimento realizada no dia 2021-11-18, para as materias primas 59, 384, 536?"
+question = "Total de vendas do mês de fevereiro de 2023"
 
 
 def get_suggested_tables(question):
     try:
-        question = QUERY.format(
-            matching_tables_str=matching_tables_str, question=question
-        )
+        question = QUERY.format(question=question)
         print(db_chain.run(question))
     except Exception as e:
         print(f"Exception: {e}")
 
 
 get_suggested_tables(question)
-
-connection.close()
-cursor.close()
