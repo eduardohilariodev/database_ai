f75ca428771cf4f5252aeb6cf4c5b154929216b4
Eduardo Hilário <eduhilario@proton.me>
2023-10-30 09:27:15 -0300

    feat(langchain_demo_chain): Replace OpenAI with LLMChain and update requirements

--- langchain_demo_chain.py
+++ langchain_demo_chain.py
@@ -6,6 +6,12 @@ env = environ.Env()
 environ.Env.read_env()
 API_KEY = env("OPENAI_API_KEY")
 
+from langchain.prompts import PromptTemplate
+from langchain import LLMChain
+
 llm = OpenAI(temperature=0, openai_api_key=API_KEY)
-question = "Which language was used to create ChatGPT?"
-print(question, llm(question))
+template = "What are the top {n} resources to learn {language} programming?"
+prompt = PromptTemplate(template=template, input_variables=["n", "language"])
+chain = LLMChain(llm=llm, prompt=prompt)
+input = {"n": 5, "language": "python"}
+print(chain.run(input))
