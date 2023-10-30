95df0e335e06b6cba32897a7d634443813b05896
Eduardo Hilário <eduhilario@proton.me>
2023-10-30 10:44:39 -0300

    refactor: Remove unused imports and functions

--- sql_database_chain.py
+++ sql_database_chain.py
@@ -1,37 +1,65 @@
+import environ
+
 from langchain.chat_models import ChatOpenAI
 from langchain_experimental.sql import SQLDatabaseChain
-from langchain.sql_database import SQLDatabase
-
-import pymysql
-import environ
+from utils.mysql_db import (
+    connect_to_database,
+    get_matching_tables,
+    get_error_tables,
+    setup_database,
+)
 
 env = environ.Env()
 environ.Env.read_env()
 API_KEY = env("OPENAI_API_KEY")
 llm = ChatOpenAI(temperature=0, openai_api_key=API_KEY)
-db = SQLDatabase.from_uri(
-    f"mysql+pymysql://{env('DB_USER')}:{env('DB_PASSWORD')}@{env('DB_HOST')}/{env('DB_SCHEMA')}"
-)
+db = setup_database()
+# connection = connect_to_database()
 
-# Setup database
-from utils.database import connect_to_database, get_matching_tables, get_error_tables
+# if connection:
+#     intended_tables = [
+#         "bi_dimensao_tempo",
+#         "bi_dimensao_tempo_diario",
+#         "bi_fato_previsao",
+#         "bi_fato_ressuprimento",
+#         "bi_fato_venda",
+#         "bi_fato_venda_cliente",
+#         "bi_fato_venda_diario",
+#     ]
 
-# Connect to the database
-connection = connect_to_database()
+#     matching_tables = get_matching_tables(intended_tables)
+#     error_tables = get_error_tables(intended_tables)
 
-if connection:
-    # Get matching tables and error tables
-    intended_tables = [
-        "bi_dimensao_tempo",
-        "bi_dimensao_tempo_diario",
-        "bi_fato_previsao",
-        "bi_fato_ressuprimento",
-        "bi_fato_venda",
-        "bi_fato_venda_cliente",
-        "bi_fato_venda_diario",
-    ]
+QUERY = """
+Given an input question, first create a syntactically correct postgresql query to run, then look at the results of the query and return the answer.
+Use the following format:
 
-    matching_tables = get_matching_tables(intended_tables)
-    error_tables = get_error_tables(intended_tables)
+Question: Question here
+SQLQuery: SQL Query to run
+SQLResult: Result of the SQLQuery
+Answer: Final answer here
 
-# Now you have 'connection', 'matching_tables', and 'error_tables' available for use.
+{question}
+"""
+
+db_chain = SQLDatabaseChain.from_llm(llm=llm, db=db, verbose=True)
+
+
+def get_prompt():
+    print("Type 'exit' to quit")
+
+    while True:
+        prompt = input("Enter a prompt: ")
+
+        if prompt.lower() == "exit":
+            print("Exiting...")
+            break
+        else:
+            try:
+                question = QUERY.format(question=prompt)
+                print(db_chain.run(question))
+            except Exception as e:
+                print(e)
+
+
+get_prompt()
